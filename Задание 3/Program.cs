ca8aff7b727502ea6a8ee7d646699fee747fc66d
Winners2006-2 <winners2006-2@mail.ru>
2023-11-20 20:59:50 +0300

    Start 1

--- Задание 3/Program.cs
+++ Задание 3/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-int last = number % 10;
+int last = number % 10; // определяем остаток от деления на 10
 int first = number;
 while (first > 9)
 {
-    first /= 10;
+    first /= 10; // делим на 10 уберая десятки из всего числа для определения первого число
 }
 int sum = first + last;
 Console.WriteLine("Сумма первой и последней цифры этого чисела: " + sum);
