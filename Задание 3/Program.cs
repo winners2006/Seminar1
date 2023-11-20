b03571e0510f4c133277d902c1fbb40a80fd3d5b
Winners2006-2 <winners2006-2@mail.ru>
2023-11-20 19:29:46 +0300

    Start

---
+++ Задание 3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int last = number % 10;
+int first = number;
+while (first > 9)
+{
+    first /= 10;
+}
+int sum = first + last;
+Console.WriteLine("Сумма первой и последней цифры этого чисела: " + sum);
