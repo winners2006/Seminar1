877414baadacbe691dd17c009b1dd5a87eaea36a
Winners2006-2 <winners2006-2@mail.ru>
2023-11-20 21:40:50 +0300

    Задание1

--- Задание 1/Program.cs
+++ Задание 1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.Write("Ведите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Ведите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a){
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+}
